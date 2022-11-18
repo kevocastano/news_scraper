a0e530d439facdd8b5fe9034b475258c5700a18a
kevo <kevo.b.castano@gmail.com>
2022-11-18 09:35:50 -0500

    Finaliza el proyectoagregando funciones en la carpeta scraper.py

--- scraper.py
+++ scraper.py
@@ -5,15 +5,38 @@ import datetime
 
 HOME_URL = 'https://www.larepublica.co/'
 
-XPATH_LINK_TO_ARTICLE = '//h2[@class]/a/@href'
+XPATH_LINK_TO_ARTICLE = '//text-fill[not(@class)]/a/@href'
 XPATH_TITLE = '//h2/span/text()'
 XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
 XPATH_BODY= '//div[@class="html-content"]/p[not(@class)]/text()'
 
 def parse_notice(link, today):
     try:
-        pass
+        response = requests.get(link)
+        if response.status_code == 200:
+            notice = response.content.decode('utf-8')
+            parsed = html.fromstring(notice)
+
+            try:
+                title = parsed.xpath(XPATH_TITLE)[0]
+                title = title.replace('\"', '')
+                summary = parsed.xpath(XPATH_SUMMARY)[0]
+                body = parsed.xpath(XPATH_BODY)
+            except IndexError:
+                return 
+
+            with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as f:
+                f.write(title)
+                f.write('\n\n')
+                f.write(summary)
+                f.write('\n\n')
+                for p in body:
+                    f.write(p)
+                    f.write('\n')
+        else:
+            raise ValueError(f'Error: {response.status_code}')
     except ValueError as ve:
+        print(ve)
 
 
 def parse_home():
